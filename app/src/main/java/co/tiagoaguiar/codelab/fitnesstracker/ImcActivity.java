9de7547e148f9ce2ea2ee1af95f2ff9581192e1d
Salomão Junio Santos Batista <salomaojunio@hotmail.com>
2021-09-14 12:34:20 -0300

    Button Reviews and Optimization
    I added some comments to organize the code and optimized the click action to hide the keyboard.

--- app/src/main/java/co/tiagoaguiar/codelab/fitnesstracker/ImcActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/fitnesstracker/ImcActivity.java
@@ -1,9 +1,12 @@
 package co.tiagoaguiar.codelab.fitnesstracker;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -30,17 +33,17 @@ public class ImcActivity extends AppCompatActivity {
         btnCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!validate()) {
+                if (!validate()) { //Caso os campos não estejam validados com valores acima de zero
                     Toast.makeText(ImcActivity.this, "Invalid values !", Toast.LENGTH_LONG).show();
                     return;
-                } else {
+                } else { //Caso os campos estejam validados com valores acima de zero
                     int height = Integer.parseInt(etxtHeight.getText().toString());
                     int weight = Integer.parseInt(etxtWeight.getText().toString());
                     double result = calculateImc(height, weight);
                     Log.d("Imc ", " " + result);
-
+                    //retorna a referencia correta de acordo com o imc, no arquivo de string(R.string...):
                     int idImcResponse = imcResponse(result);
-
+                    //Cria um AlerdDialog(um alerta)
                     AlertDialog dialog = new AlertDialog.Builder(ImcActivity.this)
                             .setTitle(getString(R.string.imc_responseStr,result))
                             .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
@@ -49,10 +52,19 @@ public class ImcActivity extends AppCompatActivity {
 
                                 }
                             })
-                            .create();
+                            .create();//Após setar as configurações, cria o alerta
                     dialog.setIcon(R.drawable.vintage_flower_icon_24dp);
                     dialog.setMessage(getString(idImcResponse));
-                    dialog.show();
+                    dialog.show(); //mostra o alerta
+
+                    //Usando InputMethodManager para esconder o teclado após aparecer o alerta:
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    //O imm é agora um gerenciador de metodo de entrada
+                    //Escondendo teclado do Height
+                    imm.hideSoftInputFromWindow(etxtHeight.getWindowToken(),0);
+                    //Escondendo teclado do Height
+                    imm.hideSoftInputFromWindow(etxtWeight.getWindowToken(),0);
+
                 }
             }
         });
